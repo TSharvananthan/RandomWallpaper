021823458123c54830aa71c6485834dd065e33f7
Thanussian Sharvananthan <48538571+TSharvananthan@users.noreply.github.com>
2020-03-20 22:10:51 -0400

    updated main to look less cluttier

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-import RandomWallpaper
-from time import sleep
+from src.RandomWallpaper import RandomWallpaper
 
-def Main():
-    works = False
-    while works == False:
-        try:
-            BACKGROUND_IMAGE_PATH = "image/background.jpg"
-            link = RandomWallpaper.MakeUrl()
-            image = RandomWallpaper.ChooseRandomImage(link)
-            RandomWallpaper.DownloadImage(image)
-            RandomWallpaper.ChangeBackground(BACKGROUND_IMAGE_PATH)
-            works = True
-        except:
-            print("ERROR, WAITING 5 SECONDS BEFORE TRYING AGAIN")
-            sleep(5)
-            continue
-
-Main()
+RandomWallpaper()
