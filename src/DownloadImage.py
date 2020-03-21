df60d5af0921075c15162daa40562b7790fcc538
Thanussian Sharvananthan <48538571+TSharvananthan@users.noreply.github.com>
2020-03-20 22:04:53 -0400

    some extra validation

--- src/DownloadImage.py
+++ src/DownloadImage.py
@@ -1,3 +1,4 @@
+import os
 from bs4 import BeautifulSoup
 from requests import get
 from urllib.request import urlretrieve
@@ -5,4 +6,9 @@ from urllib.request import urlretrieve
 def DownloadImage(url):
     object = BeautifulSoup(get(url).content, "html.parser").find("div", class_="wallpaper-resolutions").find_all("a")
     link = [x["href"] for x in object]
-    urlretrieve("http://wallpaperswide.com{}".format(link[6]), "image/background.jpg")
+    try:
+        urlretrieve("http://wallpaperswide.com{}".format(link[6]), "image/background.jpg")
+    except:
+        os.mkdir("image")
+        urlretrieve("http://wallpaperswide.com{}".format(link[6]), "image/background.jpg")
+        
